2798c4fa869b4f803016425101f25d06505ccb9b
Ananias Raphael <ananias.nobrega@gmail.com>
2020-08-29 22:29:47 -0300

    implements api

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from api import views
 
 urlpatterns = [
     path('', views.apiOverview.as_view(), name='api-overview'),
     path('tasks/', views.TaskList.as_view(), name='task-list'),
-
+    path('tasks/<str:pk>/', views.TaskDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -27,3 +26,31 @@ class TaskList(APIView):
         tasks = Task.objects.all()
         serializer = TaskSerializer(tasks, many=True)
         return Response(serializer.data)
+
+class TaskDetail(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Task.objects.get(pk=pk)
+        except Task.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        task = self.get_object(pk)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        task = self.get_object(pk)
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        task = self.get_object(pk)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
